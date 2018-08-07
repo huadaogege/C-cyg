fbc480bda0536a4b034c1df68f1640a0d7b86080
崔玉冠 <cuiyuguan@cuiyuguandeMacBook-Pro.local>
2018-08-07 22:10:49 +0800

    init C_study

---
+++ C_study/C_study/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  C_study
+//
+//  Created by 崔玉冠 on 2018/8/7.
+//  Copyright © 2018年 崔玉冠. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
